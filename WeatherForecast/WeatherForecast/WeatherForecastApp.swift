7f31c54da682314612426e0a7d088eb20e440df8
SakaiTaka23 <tt3.sakai@gmail.com>
2026-04-12 22:20:31 +0900

    created weather forecast page

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by 坂井俊文 on 2026/04/12.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
