047d14beea83efa7bed5a661def80a0009aec908
SakaiTaka23 <tt3.sakai@gmail.com>
2026-04-13 23:05:33 +0900

    pick-a-pal app

---
+++ Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by 坂井俊文 on 2026/04/13.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
